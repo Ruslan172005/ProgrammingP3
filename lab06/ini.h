210d03a69fb09f95a77e18bef0589244a6009012
Ruslan172005 <146805874+Ruslan172005@users.noreply.github.com>
2024-11-28 14:04:08 +0200

    Update ini.h

--- lab06/ini.h
+++ lab06/ini.h
@@ -6,26 +6,26 @@
 
 namespace Ini {
 
-    // Тип для зберігання секцій і ключів
+    
     using Section = std::unordered_map<std::string, std::string>;
 
     class Document {
     public:
-        // Додає нову секцію або повертає існуючу
+        
         Section& AddSection(std::string name);
 
-        // Повертає секцію за її ім'ям (константний метод)
+    
         const Section& GetSection(const std::string& name) const;
 
-        // Повертає кількість секцій у документі
+    
         size_t SectionCount() const;
 
     private:
         std::unordered_map<std::string, Section> sections;
     };
 
-    // Завантажує INI-документ із вхідного потоку
+    
     Document Load(std::istream& input);
 
-} // namespace Ini
+} 
 
